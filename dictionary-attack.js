7e435a320e7916e5dca2b7d4618048cb7e557696
tafani88 <30295505+tafani88@users.noreply.github.com>
2017-08-08 11:37:58 -0400

    Add files via upload

---
+++ dictionary-attack.js
@@ -0,0 +1,24 @@
+var wordsList = [];
+
+function init() {
+  // Load the words from the dictionary text file to wordsList
+  var wordsFile = "https://raw.githubusercontent.com/GirlsFirst/SIP-2017/master/Unit2_Applications/dictionary-attack/dictionary.txt?token=ADcVhZjRMd86ZdhPE2jVvIaJdQdzLA6Yks5YvvVSwA%3D%3D";
+  $.get(wordsFile, function(data) {
+    document.getElementById("btnSubmit").disabled = true;
+    wordsList = data.split('\n');
+    document.getElementById("btnSubmit").disabled = false;
+  });
+}
+
+window.onload = init;
+
+/* ADD YOUR CODE BELOW */
+
+function checkPassword() {
+  var password = document.getElementById("pw").value;
+    if (wordsList.indexOf(password) >=0) {
+      alert("Your password is weak!");
+  } else {
+          alert("Your password is strong!")
+    }
+  }
