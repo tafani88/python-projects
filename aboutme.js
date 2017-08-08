05ca7e9f785ec0b0b02d8676f726379a0bd6af26
tafani88 <30295505+tafani88@users.noreply.github.com>
2017-08-08 11:42:43 -0400

    Add files via upload

---
+++ aboutme.js
@@ -0,0 +1,8 @@
+window.alert("hello");
+
+//function myfunction() {
+//    document.getElementById("demo").innerHTML = ""
+
+function changebackground() {
+  document.getElementById("tafani").onclick = document.body.style.color = "blue";
+}
